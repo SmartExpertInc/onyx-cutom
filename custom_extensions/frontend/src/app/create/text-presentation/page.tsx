28364502bcc16e5c2178e6eaba0efbc2733af930
Volyn3ts <volynetskolia@gmail.com>
2025-07-09 20:09:44 +0200

    one-pager fix

--- custom_extensions/frontend/src/app/create/text-presentation/page.tsx
+++ custom_extensions/frontend/src/app/create/text-presentation/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react';
 import TextPresentationClient from './TextPresentationClient';
 
 export default function TextPresentationPage() {
-  return <TextPresentationClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <TextPresentationClient />
+    </Suspense>
+  );
 } 
