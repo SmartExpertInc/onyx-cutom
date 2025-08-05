64a47f61c613b765765c0ca422050cca97ba30ba
unknown <makshm1307@gmail.com>
2025-08-05 16:34:39 +0200

    added first forta template

--- custom_extensions/frontend/src/app/layout.tsx
+++ custom_extensions/frontend/src/app/layout.tsx
@@ -33,9 +33,9 @@ export default function RootLayout({
         <link rel="preload" href="/fonts/fonnts.com-Mont_Bold.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
 
 
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
       </head>
       <body className="h-full bg-app-gradient">
         <MontFontLoader />
