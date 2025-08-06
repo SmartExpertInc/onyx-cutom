3d7a24d6345f3ab4ef748e34afef028883d2c170
unknown <makshm1307@gmail.com>
2025-08-06 17:13:43 +0200

    fixed two-column template content text wrap

--- custom_extensions/frontend/src/components/templates/TwoColumnTemplate.tsx
+++ custom_extensions/frontend/src/components/templates/TwoColumnTemplate.tsx
@@ -282,7 +282,7 @@ export const TwoColumnTemplate: React.FC<TwoColumnProps & {
     margin: 0,
     alignSelf: 'flex-start',
     lineHeight: 1.6,
-    wordWrap: 'break-word'
+    wordWrap: 'anywhere !important'
   };
 
   return (
@@ -405,7 +405,7 @@ export const TwoColumnTemplate: React.FC<TwoColumnProps & {
                 outline: 'none',
                 resize: 'none',
                 overflow: 'hidden',
-                wordWrap: 'break-word',
+                wordWrap: 'anywhere !important',
                 whiteSpace: 'pre-wrap',
                 boxSizing: 'border-box',
                 display: 'block'
@@ -492,7 +492,7 @@ export const TwoColumnTemplate: React.FC<TwoColumnProps & {
                 outline: 'none',
                 resize: 'none',
                 overflow: 'hidden',
-                wordWrap: 'break-word',
+                wordWrap: 'anywhere !important',
                 whiteSpace: 'pre-wrap',
                 boxSizing: 'border-box',
                 display: 'block'
