b9c36c0aea8c66b2b6a5846a89e7d80f5ab2dc0b
unknown <makshm1307@gmail.com>
2025-08-12 15:21:33 +0200

    added update mark

--- custom_extensions/frontend/src/components/SmartSlideDeckViewer.tsx
+++ custom_extensions/frontend/src/components/SmartSlideDeckViewer.tsx
@@ -452,7 +452,7 @@ export const SmartSlideDeckViewer: React.FC<SmartSlideDeckViewerProps> = ({
             {componentDeck.slides.length} slide{componentDeck.slides.length !== 1 ? 's' : ''}
           </div>
         </div>
-        <h1>Hello2</h1>
+        <h1>Hello3</h1>
       </div>
       {/* Main Content Area - Static white container */}
       <div 
