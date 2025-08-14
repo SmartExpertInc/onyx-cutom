bd77cec802b6e2159350d87be14bac7b90ddd581
DmytroDD2 <vv676888@gmail.com>
2025-08-14 15:50:50 +0300

    image editing 2.1

--- custom_extensions/frontend/src/components/TextPresentationDisplay.tsx
+++ custom_extensions/frontend/src/components/TextPresentationDisplay.tsx
@@ -1440,18 +1440,21 @@ const RenderBlock: React.FC<RenderBlockProps> = (props) => {
               </div>
             )}
             
-            {/* Word-style Editor button */}
+            {/* Basic Actions Button */}
             {isEditing && (
-              <button
-                onClick={() => setShowWordStyleEditor(true)}
-                className="absolute top-2 left-2 opacity-0 group-hover:opacity-100 transition-opacity bg-[#2b579a] hover:bg-[#1e3a8a] rounded p-1.5 text-xs text-white shadow-lg z-50"
-                title="Format Picture (Word-style)"
-              >
-                <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </button>
+              <div className="absolute top-2 left-2 opacity-0 group-hover:opacity-100 transition-opacity z-50">
+                <ImageBasicActions
+                  imageBlock={block as ImageBlock}
+                  onImageChange={(updatedBlock) => {
+                    Object.keys(updatedBlock).forEach(key => {
+                      if (key !== 'type' && key !== 'src') {
+                        onTextChange?.(fieldPath(key), (updatedBlock as any)[key]);
+                      }
+                    });
+                  }}
+                  onOpenAdvancedSettings={() => setShowWordStyleEditor(true)}
+                />
+              </div>
             )}
             
             <img 
@@ -1530,18 +1533,21 @@ const RenderBlock: React.FC<RenderBlockProps> = (props) => {
               </div>
             )}
             
-            {/* Word-style Editor button */}
+            {/* Basic Actions Button */}
             {isEditing && (
-              <button
-                onClick={() => setShowWordStyleEditor(true)}
-                className="absolute top-2 left-2 opacity-0 group-hover:opacity-100 transition-opacity bg-[#2b579a] hover:bg-[#1e3a8a] rounded p-1.5 text-xs text-white shadow-lg z-50"
-                title="Format Picture (Word-style)"
-              >
-                <svg className="w-3 h-3" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </button>
+              <div className="absolute top-2 left-2 opacity-0 group-hover:opacity-100 transition-opacity z-50">
+                <ImageBasicActions
+                  imageBlock={block as ImageBlock}
+                  onImageChange={(updatedBlock) => {
+                    Object.keys(updatedBlock).forEach(key => {
+                      if (key !== 'type' && key !== 'src') {
+                        onTextChange?.(fieldPath(key), (updatedBlock as any)[key]);
+                      }
+                    });
+                  }}
+                  onOpenAdvancedSettings={() => setShowWordStyleEditor(true)}
+                />
+              </div>
             )}
             
             <img 
