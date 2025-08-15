4b8d6734a45de574300e6a8319cab2550c014554
valost <valerieostrovska@yahoo.com>
2025-08-15 14:17:56 +0300

    new page

---
+++ custom_extensions/frontend/src/app/projects-2/view/[projectId]/page.tsx
@@ -0,0 +1,13 @@
+// custom_extensions/frontend/src/app/projects-2/view/[projectId]/page.tsx
+"use client";
+
+import React from 'react';
+
+export default function Projects2ViewPage() {
+  return (
+    <div>
+      {/* Empty page - ready for implementation */}
+    </div>
+  );
+}
+
