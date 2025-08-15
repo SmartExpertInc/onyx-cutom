f455a7691ca89d5ebc9e5eb6fc11977401b5aec6
valost <valerieostrovska@yahoo.com>
2025-08-15 16:19:56 +0300

    new

--- custom_extensions/frontend/src/app/projects-2/view/page.tsx
+++ custom_extensions/frontend/src/app/projects-2/view/page.tsx
@@ -6,16 +6,17 @@ import React from 'react';
 // import { VideoPreview } from '../components/VideoPreview';
 // import { Timeline } from '../components/Timeline';
 import VideoEditorHeader from './components/VideoEditorHeader';
+import Toolbar from './components/Toolbar';
 
 export default function Projects2ViewPage() {
   return (
     <div className="min-h-screen bg-background flex flex-col">
       {/* Header */}
       <VideoEditorHeader />
-      
-      {/* Top Navigation */}
-      {/* <TopNavigation />
 
+      {/* Toolbar */}
+      <Toolbar />
+      
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col lg:flex-row overflow-hidden">
         {/* Left Sidebar - Hidden on mobile, shown on desktop */}
