23a10b7a9e856646fd78dd63e0696062b4b19faf
valost <valerieostrovska@yahoo.com>
2025-08-25 14:42:15 +0300

    toolbars

--- custom_extensions/frontend/src/app/projects-2/view/components/AdvancedSettings.tsx
+++ custom_extensions/frontend/src/app/projects-2/view/components/AdvancedSettings.tsx
@@ -48,63 +48,61 @@ export default function AdvancedSettings({
       </div>
 
       {/* Layout */}
-      <div className="flex items-center justify-between">
+      <div className="flex items-start justify-between">
         <span className="text-sm font-medium text-gray-700">Layout</span>
         <div className="flex space-x-3">
-          {/* X input with label as border */}
-          <div className="relative">
-            <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">X</label>
-            <input
-              type="number"
-              value={positionX}
-              onChange={(e) => handleNumberInput(e.target.value, onPositionXChange)}
-              className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-              placeholder="0"
-            />
+          {/* X and Width column */}
+          <div className="flex flex-col space-y-2">
+            {/* X input with label as border */}
+            <div className="relative">
+              <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">X</label>
+              <input
+                type="number"
+                value={positionX}
+                onChange={(e) => handleNumberInput(e.target.value, onPositionXChange)}
+                className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+                placeholder="0"
+              />
+            </div>
+            
+            {/* Width input with label as border */}
+            <div className="relative">
+              <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">Width</label>
+              <input
+                type="number"
+                value={width}
+                onChange={(e) => handleNumberInput(e.target.value, onWidthChange)}
+                className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+                placeholder="100"
+              />
+            </div>
           </div>
           
-          {/* Y input with label as border */}
-          <div className="relative">
-            <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">Y</label>
-            <input
-              type="number"
-              value={positionY}
-              onChange={(e) => handleNumberInput(e.target.value, onPositionYChange)}
-              className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-              placeholder="0"
-            />
-          </div>
-        </div>
-      </div>
-
-      {/* Width and Height */}
-      <div className="space-y-3">
-        <div className="flex items-center justify-between">
-          <span className="text-sm font-medium text-gray-700">Size</span>
-          <div className="flex space-x-4 text-xs text-gray-500">
-            <span>Width</span>
-            <span>Height</span>
-          </div>
-        </div>
-        
-        <div className="flex space-x-2">
-          <div className="flex-1">
-            <input
-              type="number"
-              value={width}
-              onChange={(e) => handleNumberInput(e.target.value, onWidthChange)}
-              className="w-full px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-              placeholder="100"
-            />
-          </div>
-          <div className="flex-1">
-            <input
-              type="number"
-              value={height}
-              onChange={(e) => handleNumberInput(e.target.value, onHeightChange)}
-              className="w-full px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
-              placeholder="100"
-            />
+          {/* Y and Height column */}
+          <div className="flex flex-col space-y-2">
+            {/* Y input with label as border */}
+            <div className="relative">
+              <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">Y</label>
+              <input
+                type="number"
+                value={positionY}
+                onChange={(e) => handleNumberInput(e.target.value, onPositionYChange)}
+                className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+                placeholder="0"
+              />
+            </div>
+            
+            {/* Height input with label as border */}
+            <div className="relative">
+              <label className="absolute -top-2 left-2 px-1 text-xs text-gray-600 bg-white">Height</label>
+              <input
+                type="number"
+                value={height}
+                onChange={(e) => handleNumberInput(e.target.value, onHeightChange)}
+                className="w-16 px-2 py-1 text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black focus:border-transparent"
+                placeholder="100"
+              />
+            </div>
           </div>
         </div>
       </div>
