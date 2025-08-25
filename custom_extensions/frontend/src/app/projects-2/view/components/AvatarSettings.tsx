862073b4fb6422d69ff3ced860d9cab0135a15d7
valost <valerieostrovska@yahoo.com>
2025-08-25 14:12:19 +0300

    toolbars

--- custom_extensions/frontend/src/app/projects-2/view/components/AvatarSettings.tsx
+++ custom_extensions/frontend/src/app/projects-2/view/components/AvatarSettings.tsx
@@ -234,7 +234,7 @@ export default function AvatarSettings() {
           </div>
 
           {/* Advanced Settings Toggle */}
-          <div className={`flex items-center justify-between ${!showAdvancedSettings ? '-mb-4' : ''}`}>
+          <div className={`flex items-center justify-between ${!showAdvancedSettings ? '-mb-4 mb-[5px]' : ''}`}>
             <span className="text-xs text-gray-500">
               {showAdvancedSettings ? 'Collapse advanced settings' : 'Show advanced settings'}
             </span>
