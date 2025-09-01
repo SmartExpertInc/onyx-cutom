5654147043b6f7d9288fb7b399ac323895410b3d
dariakulish1 <dashaklsh1507@gmail.com>
2025-09-01 13:46:13 +0300

    small fixes in Role btn

--- custom_extensions/frontend/src/app/projects/view/[projectId]/page.tsx
+++ custom_extensions/frontend/src/app/projects/view/[projectId]/page.tsx
@@ -1480,7 +1480,7 @@ export default function ProjectInstanceViewPage() {
                         />
                         <button
                           onClick={handleAddEmail}
-                          disabled={!isValidEmail(newEmail)}
+                          // disabled={!isValidEmail(newEmail)}
                           className="px-3 py-1.5 text-sm font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
                         >
                           {t('interface.projectView.add', 'Add')}
