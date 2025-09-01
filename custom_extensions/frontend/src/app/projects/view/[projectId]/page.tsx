3e4508a5de8cb825241203c18eebda31083a1326
dariakulish1 <dashaklsh1507@gmail.com>
2025-09-01 18:19:45 +0300

    removed dropdown

--- custom_extensions/frontend/src/app/projects/view/[projectId]/page.tsx
+++ custom_extensions/frontend/src/app/projects/view/[projectId]/page.tsx
@@ -1427,7 +1427,6 @@ export default function ProjectInstanceViewPage() {
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" />
                   </svg>
                   {t('interface.projectView.Role', 'Role')}
-                  <ChevronDown size={16} className="ml-1" />
                 </button>
 
                 {/* Role Access Modal */}
