512f026a8bf26271757421779097c3f421be1e78
valost <valerieostrovska@yahoo.com>
2025-09-10 17:38:01 +0300

    audit

--- custom_extensions/frontend/src/app/create/commercial-proposal/page.tsx
+++ custom_extensions/frontend/src/app/create/commercial-proposal/page.tsx
@@ -1147,7 +1147,7 @@ export default function CommercialProposalPage() {
                 </div>
 
                 <div 
-                  className="flex flex-col gap-[5px] xl:gap-[20px] py-[30px] xl:py-[40px] xl:my-[15px] items-center justify-center text-center"
+                  className="flex flex-col gap-[5px] xl:gap-[20px] py-[30px] xl:py-[40px] xl:my-[20px] items-center justify-center text-center"
                   style={{
                     background: 'linear-gradient(90deg, rgba(255, 255, 255, 0) 0%, #EFF4FF 100%)',
                     borderRadius: '6px',
@@ -1295,7 +1295,7 @@ export default function CommercialProposalPage() {
                 Тариф:
               </h4>
 
-              <div className="flex flex-col xl:flex-row gap-[10px] xl:gap-0 xl:border xl:border-[#E0E0E0] xl:rounded-[6px] xl:shadow-[0px_24px_24px_-8px_#2A334608] mb-[5px] xl:mb-[40px]">
+              <div className="flex flex-col xl:flex-row gap-[10px] xl:gap-0 xl:border xl:border-[#E0E0E0] xl:rounded-[6px] xl:shadow-[0px_24px_24px_-8px_#2A334608] mb-[5px]">
                 <div 
                   className="xl:w-[500px] rounded-[6px] bg-[#F5F8FF] xl:bg-[linear-gradient(240.17deg,#F5F8FF_34.29%,rgba(203,220,255,0.55)_107.22%)] px-[15px] xl:px-[30px] py-[20px] xl:py-[30px] flex flex-col gap-[20px] flex-shrink-0"
                 >
