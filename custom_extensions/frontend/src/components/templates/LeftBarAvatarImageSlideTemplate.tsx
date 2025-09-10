dc0d12a8aed27fc6658eeb56fe69540d73d2c23e
Евгений Чупахин <chipserso@gmail.com>
2025-09-10 17:59:09 +0300

    new Update

--- custom_extensions/frontend/src/components/templates/LeftBarAvatarImageSlideTemplate.tsx
+++ custom_extensions/frontend/src/components/templates/LeftBarAvatarImageSlideTemplate.tsx
@@ -22,7 +22,7 @@ export const LeftBarAvatarImageSlideTemplate: React.FC<LeftBarAvatarImageProps &
 
   const slide: React.CSSProperties = { width:'100%', aspectRatio:'16/9', background:'#FFFFFF', fontFamily: currentTheme.fonts.titleFont, position:'relative', overflow:'hidden' };
   const leftBar: React.CSSProperties = { position:'absolute', left:0, top:0, bottom:0, width:'190px', background:'#000000' };
-  const avatarWrap: React.CSSProperties = { position:'absolute', left:'118px', zIndex:'10', top:'265px', width:'230px', height:'230px', borderRadius:'50%', overflow:'hidden', background:'#F9FAFE' };
+  const avatarWrap: React.CSSProperties = { position:'absolute', left:'118px', zIndex:'10', top:'50%', transform:'translateY(-50%)', width:'230px', height:'230px', borderRadius:'50%', overflow:'hidden', background:'#F9FAFE' };
   const frame: React.CSSProperties = { position:'absolute', left:'330px', top:'100px', right:'70px', bottom:'100px', border:'1px solid #000000' };
 
   return (
