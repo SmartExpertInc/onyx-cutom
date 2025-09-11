918ab3a3892755a3db2bf9ad5763e61ab7e5e877
Danil49 <58887592+Danil49@users.noreply.github.com>
2025-09-11 16:07:11 +0200

    fix: trailing slash

--- custom_extensions/frontend/src/lib/mixpanelClient.ts
+++ custom_extensions/frontend/src/lib/mixpanelClient.ts
@@ -17,7 +17,7 @@ export const initMixpanel = (): void => {
 
   // Use the Config type from @types/mixpanel-browser
   const options: Partial<Config> = {
-    api_host: '/tracking-proxy',
+    api_host: '/tracking-proxy/',
     debug: true,
     track_pageview: true,
   };
