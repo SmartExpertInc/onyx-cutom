e6d2bae85741085e109d91166b48004e956a493c
valost <valerieostrovska@yahoo.com>
2025-09-12 21:16:27 +0300

    fixing

--- custom_extensions/frontend/src/app/create/quiz/QuizClient.tsx
+++ custom_extensions/frontend/src/app/create/quiz/QuizClient.tsx
@@ -1378,7 +1378,7 @@ export default function QuizClient() {
               />
               <Edit 
                 size={16} 
-                className="absolute top-[20px] right-7 text-gray-400 pointer-events-none flex items-center justify-center" 
+                className="absolute top-[30px] right-7 text-gray-400 pointer-events-none flex items-center justify-center" 
               />
             </div>
           )}
