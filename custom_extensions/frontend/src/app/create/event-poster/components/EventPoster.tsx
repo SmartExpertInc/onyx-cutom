32e3af0ebb0cd486afee2319e28c7ec61ed4997f
MaksymKratasiuk <makshm1307@gmail.com>
2025-09-18 14:56:45 +0200

    changed structure

--- custom_extensions/frontend/src/app/create/event-poster/components/EventPoster.tsx
+++ custom_extensions/frontend/src/app/create/event-poster/components/EventPoster.tsx
@@ -46,7 +46,7 @@ export default function EventPoster({
         boxSizing: 'border-box'
       }}
     >
-      {/* Main background - v1_5 without left positioning */}
+      {/* Main background - v1_5 */}
       <div
         className="absolute inset-0"
         style={{
@@ -54,21 +54,6 @@ export default function EventPoster({
           backgroundRepeat: 'no-repeat',
           backgroundPosition: 'center center',
           backgroundSize: 'cover',
-        }}
-      />
-
-      {/* Logo - v1_6 - keeping as absolute */}
-      <div
-        className="absolute"
-        style={{
-          width: '141px',
-          height: '78px',
-          background: 'url("/custom-projects-ui/create/event-poster/figma-to-html/images/v1_6.png")',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center center',
-          backgroundSize: 'cover',
-          top: '79px',
-          left: '776px',
         }}
       />
 
@@ -87,7 +72,7 @@ export default function EventPoster({
         }}
       />
 
-      {/* Bottom gradient - v1_10 - keeping or replacing with CSS gradient */}
+      {/* Bottom gradient */}
       <div
         className="absolute"
         style={{
@@ -99,11 +84,15 @@ export default function EventPoster({
         }}
       />
 
-      {/* Content structure using normal flow */}
-      <div className="relative z-10 h-full flex flex-col">
-        {/* Header section */}
-        <div className="flex justify-between items-start" style={{ marginTop: '58px', marginLeft: '53px', marginRight: '53px' }}>
-          <div className="flex-1" style={{ maxWidth: '515px' }}>
+      {/* Main content grid layout */}
+      <div className="relative z-10 h-full" style={{ display: 'grid', gridTemplateColumns: '1fr auto', gridTemplateRows: 'auto 1fr auto', padding: '53px' }}>
+        
+        {/* Header section - spanning both columns */}
+        <div className="col-span-2" style={{ display: 'flex', flexDirection: 'column', gap: '40px', marginBottom: '40px' }}>
+          
+          {/* First row: Event name and Logo */}
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            {/* Event name */}
             <div
               style={{
                 color: 'rgba(235,235,235,1)',
@@ -116,128 +105,138 @@ export default function EventPoster({
             >
               {eventName}:
             </div>
+
+            {/* Logo */}
+            <div
+              style={{
+                width: '141px',
+                height: '78px',
+                background: 'url("/custom-projects-ui/create/event-poster/figma-to-html/images/v1_6.png")',
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center center',
+                backgroundSize: 'cover'
+              }}
+            />
           </div>
-          
-          {/* Date section with CSS border instead of v1_7 */}
-          <div 
-            className="text-right" 
-            style={{ 
-              marginTop: '133px', 
-              marginRight: '54px',
-              border: '3px solid rgba(255,255,255,0.8)',
-              borderRadius: '15px',
-              padding: '15px 20px',
-              background: 'rgba(0,0,0,0.2)',
-              backdropFilter: 'blur(5px)'
-            }}
-          >
-            <div
-              style={{
-                color: 'rgba(255,255,255,1)',
-                fontFamily: 'Montserrat',
-                fontWeight: '600',
-                fontSize: '58px',
-                textAlign: 'left',
-                lineHeight: '1'
-              }}
-            >
-              {dayMonth}
+
+          {/* Second row: Speaker info and Date */}
+          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+            {/* Speaker name and description */}
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '20px', maxWidth: '600px' }}>
+              <div
+                style={{
+                  color: 'rgba(235,235,235,1)',
+                  fontFamily: 'Montserrat',
+                  fontWeight: '600',
+                  fontSize: '41px',
+                  textAlign: 'left',
+                  lineHeight: '1.2'
+                }}
+              >
+                {mainSpeaker}
+              </div>
+              
+              <div
+                style={{
+                  color: 'rgba(235,235,235,1)',
+                  fontFamily: 'Montserrat',
+                  fontWeight: '400',
+                  fontSize: '20px',
+                  textAlign: 'left',
+                  lineHeight: '1.2'
+                }}
+              >
+                {speakerDescription}
+              </div>
             </div>
-            <div
-              style={{
-                color: 'rgba(255,255,255,1)',
-                fontFamily: 'Montserrat',
-                fontWeight: '300',
-                fontSize: '52px',
-                textAlign: 'left',
-                lineHeight: '1',
-                marginTop: '9px',
-                marginLeft: '23px'
+
+            {/* Date with border */}
+            <div 
+              style={{ 
+                border: '3px solid rgba(255,255,255,0.8)',
+                borderRadius: '15px',
+                padding: '15px 20px',
+                background: 'rgba(0,0,0,0.2)',
+                backdropFilter: 'blur(5px)',
+                display: 'inline-block'
               }}
             >
-              {year}
+              <div
+                style={{
+                  color: 'rgba(255,255,255,1)',
+                  fontFamily: 'Montserrat',
+                  fontWeight: '600',
+                  fontSize: '58px',
+                  textAlign: 'center',
+                  lineHeight: '1'
+                }}
+              >
+                {dayMonth}
+              </div>
+              <div
+                style={{
+                  color: 'rgba(255,255,255,1)',
+                  fontFamily: 'Montserrat',
+                  fontWeight: '300',
+                  fontSize: '52px',
+                  textAlign: 'center',
+                  lineHeight: '1',
+                  marginTop: '5px'
+                }}
+              >
+                {year}
+              </div>
             </div>
           </div>
         </div>
 
-        {/* Speaker section */}
-        <div style={{ marginTop: '47px', marginLeft: '52px' }}>
+        {/* Left content area */}
+        <div style={{ display: 'flex', flexDirection: 'column', gap: '40px', paddingRight: '50px' }}>
+          {/* Topic */}
           <div
             style={{
-              width: '429px',
               color: 'rgba(235,235,235,1)',
               fontFamily: 'Montserrat',
               fontWeight: '600',
-              fontSize: '41px',
+              fontSize: '50px',
               textAlign: 'left',
-              lineHeight: '1.2'
+              lineHeight: '1.2',
+              maxWidth: '480px'
             }}
           >
-            {mainSpeaker}
+            {topic}
           </div>
-          
+
+          {/* Additional Speakers */}
           <div
             style={{
-              width: '645px',
               color: 'rgba(235,235,235,1)',
               fontFamily: 'Montserrat',
               fontWeight: '400',
               fontSize: '20px',
               textAlign: 'left',
               lineHeight: '1.2',
-              marginTop: '28px'
-            }}
-          >
-            {speakerDescription}
-          </div>
-        </div>
-
-        {/* Topic section */}
-        <div style={{ marginTop: '127px', marginLeft: '53px' }}>
-          <div
-            style={{
-              width: '480px',
-              color: 'rgba(235,235,235,1)',
-              fontFamily: 'Montserrat',
-              fontWeight: '600',
-              fontSize: '50px',
-              textAlign: 'left',
-              lineHeight: '1.2'
-            }}
-          >
-            {topic}
-          </div>
-        </div>
-
-        {/* Additional speakers section */}
-        <div style={{ marginTop: '255px', marginLeft: '53px' }}>
-          <div
-            style={{
-              width: '460px',
-              color: 'rgba(235,235,235,1)',
-              fontFamily: 'Montserrat',
-              fontWeight: '400',
-              fontSize: '20px',
-              textAlign: 'left',
-              lineHeight: '1.2'
+              maxWidth: '460px'
             }}
           >
             {additionalSpeakers}
           </div>
         </div>
 
-        {/* Bottom section with ticket info */}
-        <div className="flex items-center justify-between mt-auto" style={{ marginBottom: '56px', marginLeft: '53px', marginRight: '53px' }}>
-          {/* Ticket price section with CSS border instead of v1_13 */}
+        {/* Right area - empty space for speaker image */}
+        <div></div>
+
+        {/* Bottom section - spanning both columns */}
+        <div className="col-span-2" style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginTop: 'auto', paddingTop: '50px' }}>
+          {/* Ticket price section */}
           <div 
-            className="flex flex-col" 
             style={{ 
-              width: '189px',
               border: '2px solid rgba(235,235,235,0.6)',
               borderRadius: '12px',
-              padding: '15px',
+              padding: '15px 20px',
               background: 'rgba(0,0,0,0.3)',
-              backdropFilter: 'blur(3px)'
+              backdropFilter: 'blur(3px)',
+              minWidth: '150px'
             }}
           >
             <div
@@ -277,32 +276,25 @@ export default function EventPoster({
             </div>
           </div>
 
-          {/* Free access conditions with CSS styling instead of v1_11 and v1_12 */}
-          <div 
-            className="flex-1" 
-            style={{ 
-              marginLeft: '95px', 
-              marginTop: '13px',
+          {/* Free access conditions */}
+          <div
+            style={{
+              color: 'rgba(235,235,235,1)',
+              fontFamily: 'Roboto',
+              fontWeight: '400',
+              fontSize: '37px',
+              textAlign: 'center',
+              lineHeight: '1.2',
+              background: 'rgba(0,0,0,0.6)',
+              borderRadius: '25px',
+              padding: '20px 30px',
+              boxShadow: '0 0 30px rgba(255,255,255,0.2), 0 0 60px rgba(255,255,255,0.1)',
+              backdropFilter: 'blur(5px)',
+              maxWidth: '500px',
+              marginLeft: '30px'
             }}
           >
-            <div
-              style={{
-                width: '510px',
-                color: 'rgba(235,235,235,1)',
-                fontFamily: 'Roboto',
-                fontWeight: '400',
-                fontSize: '37px',
-                textAlign: 'center',
-                lineHeight: '1.2',
-                background: 'rgba(0,0,0,0.6)',
-                borderRadius: '25px',
-                padding: '20px 30px',
-                boxShadow: '0 0 30px rgba(255,255,255,0.2), 0 0 60px rgba(255,255,255,0.1)',
-                backdropFilter: 'blur(5px)'
-              }}
-            >
-              {freeAccessConditions}
-            </div>
+            {freeAccessConditions}
           </div>
         </div>
       </div>
