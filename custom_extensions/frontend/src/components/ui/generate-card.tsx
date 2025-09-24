b2470422640c0e9a3fe467907548c21be9f9bdcb
valost <valerieostrovska@yahoo.com>
2025-09-24 18:21:34 +0300

    fixes

--- custom_extensions/frontend/src/components/ui/generate-card.tsx
+++ custom_extensions/frontend/src/components/ui/generate-card.tsx
@@ -31,11 +31,11 @@ const GenerateCard = React.forwardRef<HTMLDivElement, GenerateCardProps>(
             {Icon && (
               <div className="flex items-center justify-center" style={{ color: '#FFFFFF' }}>
                 <Icon 
-                  size={40}
-                  strokeWidth={3}
+                  size={48}
+                  strokeWidth={4}
                   className={cn(
                     "text-white",
-                    "w-10 h-10 xs:w-10 xs:h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-12 lg:h-12 xl:w-12 xl:h-12"
+                    "w-12 h-12 xs:w-12 xs:h-12 sm:w-14 sm:h-14 md:w-16 md:h-16 lg:w-16 lg:h-16 xl:w-16 xl:h-16"
                   )}
                   style={{
                     color: '#FFFFFF',
