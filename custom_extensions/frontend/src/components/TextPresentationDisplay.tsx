e980c723cc3695e6af52d4af8fd6fec23f8b5e10
dariakulish1 <dashaklsh1507@gmail.com>
2025-09-26 17:08:51 +0300

    small changes

--- custom_extensions/frontend/src/components/TextPresentationDisplay.tsx
+++ custom_extensions/frontend/src/components/TextPresentationDisplay.tsx
@@ -1074,7 +1074,7 @@ const RenderBlock: React.FC<RenderBlockProps> = (props) => {
 
           {/* List icon picker for bullet lists */}
           {isEditing && !!onTextChange && !isNumbered && (
-            <div className="absolute top-1 left-2 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-100 text-gray-900 border border-gray-300 rounded px-2 py-1 text-xs z-40 flex gap-1">
+            <div className="absolute top-0 left-3 opacity-0 group-hover:opacity-100 transition-opacity bg-gray-100 text-gray-900 border border-gray-300 rounded px-2 py-1 text-xs z-40 flex gap-1">
               <div className="relative">
                 <button 
                   className="p-1 rounded hover:bg-gray-200" 
