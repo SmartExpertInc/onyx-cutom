e4e1536e41c0ab4046047098b0db9290ac715032
Евгений Чупахин <chipserso@gmail.com>
2025-10-30 15:37:56 +0200

    Fix Slides in Fronted and HTML

--- custom_extensions/frontend/src/components/templates/PercentCirclesSlideTemplate.tsx
+++ custom_extensions/frontend/src/components/templates/PercentCirclesSlideTemplate.tsx
@@ -1,11 +1,11 @@
 // custom_extensions/frontend/src/components/templates/PercentCirclesSlideTemplate.tsx
 
 import React from 'react';
-import { PercentCirclesSlideProps } from '@/types/slideTemplates';
+import { PercentCirclesProps } from '@/types/slideTemplates';
 import { SlideTheme } from '@/types/slideThemes';
 import PercentCirclesSlideTemplate_old from './PercentCirclesSlideTemplate_old';
 
-export const PercentCirclesSlideTemplate: React.FC<PercentCirclesSlideProps & {
+export const PercentCirclesSlideTemplate: React.FC<PercentCirclesProps & {
   theme?: SlideTheme | string;
 }> = (props) => {
   return <PercentCirclesSlideTemplate_old {...props} />;
