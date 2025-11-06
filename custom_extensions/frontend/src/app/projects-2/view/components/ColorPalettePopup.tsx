2252a5848937fea025fe7d0c8a5935b650299433
valost <valerieostrovska@yahoo.com>
2025-11-06 15:24:45 +0200

    updating video constructor

--- custom_extensions/frontend/src/app/projects-2/view/components/ColorPalettePopup.tsx
+++ custom_extensions/frontend/src/app/projects-2/view/components/ColorPalettePopup.tsx
@@ -874,8 +874,8 @@ const ColorPalettePopup: React.FC<ColorPalettePopupProps> = ({
                                  linear-gradient(-45deg, #ccc 25%, transparent 25%), 
                                  linear-gradient(45deg, transparent 75%, #ccc 75%), 
                                  linear-gradient(-45deg, transparent 75%, #ccc 75%)`,
-                    backgroundSize: '12px 12px',
-                    backgroundPosition: '0 0, 0 6px, 6px -6px, -6px 0px'
+                    backgroundSize: '16px 16px',
+                    backgroundPosition: '0 0, 0 8px, 8px -8px, -8px 0px'
                   }}
                 />
               <VerticalSlider
