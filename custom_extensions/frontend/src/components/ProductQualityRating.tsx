812b09197f1f76808007407ada89b7f70c59fae7
valost <valerieostrovska@yahoo.com>
2025-11-14 17:20:12 +0200

    updating view link ui for mobile

--- custom_extensions/frontend/src/components/ProductQualityRating.tsx
+++ custom_extensions/frontend/src/components/ProductQualityRating.tsx
@@ -31,7 +31,7 @@ const ProductQualityRating = ({
 
   const ratingBoxClasses = [
     'items-center gap-3 bg-[#FFFFFF] border border-[#E0E0E0] shadow-xl rounded-md px-3 py-3',
-    fullWidth ? 'flex w-full' : 'inline-flex',
+    fullWidth ? 'flex w-full justify-center' : 'inline-flex',
   ]
     .filter(Boolean)
     .join(' ');
